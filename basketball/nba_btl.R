a957d63ddacc5379fbd2e33fa45740d300b32d13
Christopher D. Long <galizur@gmail.com>
2016-06-02 16:42:20 -0400

    Remove references to Postgres

--- basketball/nba_btl.R
+++ basketball/nba_btl.R
@@ -2,25 +2,6 @@ library("BradleyTerry2")
 
 games <- read.csv("nba_2016.csv",stringsAsFactors=FALSE)
 
-#library("RPostgreSQL")
-
-#drv <- dbDriver("PostgreSQL")
-
-#con <- dbConnect(drv, dbname="basketball")
-
-#query <- dbSendQuery(con, "
-#select
-#team_id as team,
-#opponent_id as opponent,
-#team_score as team_score,
-#opponent_score as opponent_score,
-#(case when team_score>opponent_score then TRUE
-# else FALSE end) as outcome
-#from bbref.results
-#where year=2016
-#;")
-
-#games <- fetch(query,n=-1)
 dim(games)
 
 fit <- BTm(outcome,team,opponent,data=games)
