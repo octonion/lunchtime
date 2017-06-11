e30806d71b406b3e1d24d629c46a5857040c25f9
Christopher D. Long <galizur@gmail.com>
2017-06-11 19:17:59 -0400

    NHL 1973-1980

---
+++ pythagorean/nhl_early.R
@@ -0,0 +1,48 @@
+library("BradleyTerry2")
+
+library("RPostgreSQL")
+
+drv <- dbDriver("PostgreSQL")
+
+con <- dbConnect(drv, dbname="hockey")
+
+query <- dbSendQuery(con, "
+select
+team_id as team,
+opponent_id as opponent,
+team_score as team_score,
+opponent_score as opponent_score,
+(case
+ when team_score>opponent_score then 1.0
+ when team_score=opponent_score then 0.5
+ else 0.0 end) as outcome
+from href.results
+where year=1975
+;")
+
+games <- fetch(query,n=-1)
+dim(games)
+
+fit <- BTm(outcome, team, opponent, data=games)
+
+fit
+
+out <- as.data.frame(BTabilities(fit))
+out <- out[with(out, order(-ability)), ]
+
+out <- subset(out, TRUE, select=c(ability))
+out$ability <- exp(out$ability)
+
+out$team <- rownames(out)
+out
+
+scored <- aggregate(team_score ~ team, games, sum)
+allowed <- aggregate(opponent_score ~ team, games, sum)
+
+df <- merge(out,scored)
+df <- merge(df,allowed)
+
+fit <- lm(log(ability) ~ log(team_score/opponent_score), df)
+
+fit
+quit("no")
